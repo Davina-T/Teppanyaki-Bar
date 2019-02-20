eb719bcc4c577852fa1f8697a3df1a8dce2ce349
Davina <36658212+Davina-T@users.noreply.github.com>
2019-02-20 12:37:23 +1000

    added favicon and automatic navbar close on mobile

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+// Automatically Hide Navbar in Dropdown
+
+$('.navbar-nav li a').on('click', function(){
+    if(!$( this ).hasClass('dropdown-toggle')){
+        $('.navbar-collapse').collapse('hide');
+    }
+});
+
 // Active Navbar Effect
 
 $(document).ready(function() {
